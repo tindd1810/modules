7623c06d945ce912ead96466214082930bde08ae
hoangdl <hoangledinh65@gmail.com>
2022-07-16 11:01:42 +0700

    update

--- services/key.tf
+++ services/key.tf
@@ -1,10 +1,5 @@
-resource "aws_key_pair" "publickey" {
+resource "aws_key_pair" "prod-publickey" {
   count = var.isProd
-  key_name = "hoangdl-publickey"
+  key_name = "hoangdl-prod-publickey"
   public_key = file("${path.module}/prod-publickey")
-}
-resource "aws_key_pair" "publickey" {
-  count = var.isTesting
-  key_name = "hoangdl-publickey"
-  public_key = file("${path.module}/testing-publickey")
 }
