14de552b13ad24670f07736806e9e57c0705f572
hoangdl <hoangledinh65@gmail.com>
2022-07-16 10:05:26 +0700

    update

--- vpc/main.tf
+++ vpc/main.tf
@@ -23,8 +23,8 @@ resource "aws_security_group_rule" "HTTP-traffic" {
 
 resource "aws_security_group_rule" "SSH-traffic" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 222
+  to_port           = 222
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.hoangdl-sg.id
