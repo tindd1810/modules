bc55c7188ebd273608cf852b12c65389ed494314
hoangdl <hoangledinh65@gmail.com>
2022-07-18 07:02:56 +0700

    update

--- services/var.tf
+++ services/var.tf
@@ -5,6 +5,10 @@ variable "target-group-name" {
   description = "target-group-name"
 }
 
+variable "lambda-function-name" {
+  description = "Lambda function's name"
+}
+
 variable "env" {
   description = "environment"
 }
