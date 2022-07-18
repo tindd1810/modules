cb8917400be548ceaf106cc2103fde9ac3b7d456
hoangdl <hoangledinh65@gmail.com>
2022-07-18 17:11:57 +0700

    update

--- services/loadbalancer.tf
+++ services/loadbalancer.tf
@@ -30,64 +30,64 @@ resource "aws_lb_target_group_attachment" "ec2-1" {
   target_id        = aws_instance.hoangdl-amz-ec2[1].id
   port             = 80
 }
-resource "aws_lb_target_group_attachment" "lambda-1" {
-  target_group_arn = aws_lb_target_group.lambda-tg.arn
-  target_id        = aws_lambda_function.test_lambda.arn
-  depends_on       = [aws_lambda_permission.with_lb]
-  port             = 80
-}
-
-
-# resource "aws_lb_listener" "hoangdl" {
-#   load_balancer_arn = aws_lb.hoangdl-alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ec2-tg.arn
-#   }
+# resource "aws_lb_target_group_attachment" "lambda-1" {
+#   target_group_arn = aws_lb_target_group.lambda-tg.arn
+#   target_id        = aws_lambda_function.test_lambda.arn
+#   depends_on       = [aws_lambda_permission.with_lb]
+#   port             = 80
 # }
-resource "aws_lb_listener" "lambda" {
+
+
+resource "aws_lb_listener" "hoangdl" {
   load_balancer_arn = aws_lb.hoangdl-alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.lambda-tg.arn
+    target_group_arn = aws_lb_target_group.ec2-tg.arn
   }
 }
+# resource "aws_lb_listener" "lambda" {
+#   load_balancer_arn = aws_lb.hoangdl-alb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-# resource "aws_lb_listener_rule" "static" {
-#   listener_arn = aws_lb_listener.hoangdl.arn
-#   priority     = 100
-
-#   action {
+#   default_action {
 #     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ec2-tg.arn
+#     target_group_arn = aws_lb_target_group.lambda-tg.arn
 #   }
-
-#   condition {
-#     path_pattern {
-#       values = ["/test*"]
-#     }
-#   }
-
 # }
-resource "aws_lb_listener_rule" "lambda" {
-  listener_arn = aws_lb_listener.lambda.arn
-  priority     = 101
+
+resource "aws_lb_listener_rule" "static" {
+  listener_arn = aws_lb_listener.hoangdl.arn
+  priority     = 100
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.lambda-tg.arn
+    target_group_arn = aws_lb_target_group.ec2-tg.arn
   }
 
   condition {
     path_pattern {
-      values = ["/search*"]
+      values = ["/test*"]
     }
   }
 
 }
+# resource "aws_lb_listener_rule" "lambda" {
+#   listener_arn = aws_lb_listener.lambda.arn
+#   priority     = 101
+
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.lambda-tg.arn
+#   }
+
+#   condition {
+#     path_pattern {
+#       values = ["/search*"]
+#     }
+#   }
+
+# }
