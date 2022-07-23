5d5540766f0ad036fe3dbd34fd4b746f006937ed
hoangdl <hoangledinh65@gmail.com>
2022-07-23 08:42:25 +0700

    update

--- jenkins-node/userdata.sh
+++ jenkins-node/userdata.sh
@@ -3,4 +3,5 @@ apt update -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-amazon-linux-extras install java-openjdk11
+amazon-linux-extras install java-openjdk11 -y
+yum install git -y
