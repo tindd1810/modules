cbe06ec1a6a8e80c20dca6a314cf57981afdd566
tindd1810 <duytin1810@gmail.com>
2022-08-14 17:36:11 +0700

    update userdata

--- services/userdata.sh
+++ services/userdata.sh
@@ -2,4 +2,4 @@
 apt update
 apt install nginx -y 
 systemctl start nginx
-echo $'curl http://169.254.169.254/latest/meta-data/public-ipv4' > /var/www/html/index.nginx-debian.html
+echo $(curl http://169.254.169.254/latest/meta-data/public-ipv4) > /var/www/html/index.nginx-debian.html
