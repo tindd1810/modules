f569f99b66ef3fb89fcd953ffc0582aa315ec715
tindd1810 <duytin1810@gmail.com>
2022-08-14 17:08:50 +0700

    update lb

--- services/loadbalancer.tf
+++ services/loadbalancer.tf
@@ -48,16 +48,16 @@ resource "aws_lb_listener" "tindd" {
     target_group_arn = aws_lb_target_group.ec2-tg.arn
   }
 }
-resource "aws_lb_listener" "lambda" {
-  load_balancer_arn = aws_lb.hoangdl-alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "lambda" {
+#   load_balancer_arn = aws_lb.hoangdl-alb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.lambda-tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.lambda-tg.arn
+#   }
+# }
 
 resource "aws_lb_listener_rule" "static" {
   listener_arn = aws_lb_listener.tindd.arn
@@ -74,20 +74,20 @@ resource "aws_lb_listener_rule" "static" {
     }
   }
 
-}
-resource "aws_lb_listener_rule" "lambda" {
-  listener_arn = aws_lb_listener.lambda.arn
-  priority     = 101
+# }
+# resource "aws_lb_listener_rule" "lambda" {
+#   listener_arn = aws_lb_listener.lambda.arn
+#   priority     = 101
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.lambda-tg.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.lambda-tg.arn
+#   }
 
-  condition {
-    path_pattern {
-      values = ["/search*"]
-    }
-  }
+#   condition {
+#     path_pattern {
+#       values = ["/search*"]
+#     }
+#   }
 
-}
+# }
